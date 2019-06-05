912bb32df05e60d1484d755d9f6dd1efed796103
Jac <jacalata@users.noreply.github.com>
2019-06-04 17:58:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var PORT = 3000;
 var os = require("os");
 var hostName = process.env.hostname || os.hostname(); // for local dev use replace this with "localhost"
 var protocol = "";
-if (!hostName.startsWith("http")) { protocol = "http://"; 
+if (!hostName.startsWith("http")) { protocol = "http://"; }
 var redirectUri = protocol + hostName + ":" + PORT + "/callback";
 
 module.exports = {
